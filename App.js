b447181eb31b3b122157b928dfd345c8b61bf8a9
claxonode <117821208+claxonode@users.noreply.github.com>
2024-02-15 21:23:23 +1100

    Can loop sound but I cannot control interval yet

--- App.js
+++ App.js
@@ -1,30 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { useEffect,useState } from 'react';
+import { useEffect,useState,useRef } from 'react';
 import {Audio} from 'expo-av'
+import Slider from '@react-native-community/slider';
 
 export default function App() {
   const [sound, setSound] = useState();
-  async function playAudioClip() {
-    console.log("Loading Sound");
+  const [bpm,setBpm] = useState(180)
+  const [sliderValue,setSliderValue] = useState(0)
+  const [metronome,setMetronome] = useState(false)
+
+
+  // async function playAudioClip() {
+  //   console.log("Loading Sound");
+    
+  //   setSound(sound)
+  //   console.log("Playing Sound");
+  //   await sound.playAsync();
+  // }
+  function stopMetronome() {
+    setMetronome(false) 
+    sound.unloadAsync()
+  }
+  async function playMetronome() {
     const {sound} = await Audio.Sound.createAsync(require('./assets/bubble.wav'));
     setSound(sound)
-    console.log("Playing Sound");
-    await sound.playAsync();
+    // setMetronome(true)
+    sound.playAsync()
+    sound.setIsLoopingAsync(true)
   }
 
-  useEffect(()=>{
-    return sound ? ()=> {
-      console.log('Unloading Sound')
-      sound.unloadAsync()
-    }: undefined
-  },[sound])
+  // while (metronome === true) {
+  //   setTimeout(async()=> {
+  //     await sound.playAsync()
+  //   },500)
+  // }
+  // useEffect(()=>{
+  //   return sound ? ()=> {
+  //     console.log('Unloading Sound')
+  //     sound.unloadAsync()
+  //   }: undefined
+  // },[sound])
 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Button title='Play Sound' onPress={playAudioClip} ></Button>
+      <Button title='Play Metronome' onPress={playMetronome} ></Button>
+      <Button title='Stop Metronome' onPress={stopMetronome} ></Button>
       <StatusBar style="auto" />
+
+      <Slider 
+        style={{width:200,height:40}} 
+        minimumValue={120} maximumValue={200} step={1} onValueChange={(value)=>{setBpm(value)}}
+        minimumTrackTintColor='#000000' maximumTrackTintColor="#000000"
+      />
+      <Text>Bpm: {bpm}</Text>
+      <Slider 
+        style={{width:200,height:40}} 
+        minimumValue={0} maximumValue={1} onValueChange={(value)=>{setSliderValue(value)}}
+        minimumTrackTintColor='#000000' maximumTrackTintColor="#000000"
+      />
+      <Text>Slider value: {sliderValue}</Text>
     </View>
   );
 }
