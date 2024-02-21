cfada7c6412861a3437ab5ebf47d31107e70f7f1
claxonode <117821208+claxonode@users.noreply.github.com>
2024-02-22 00:41:45 +1100

    Styled app with react-native-vector-icons/materialicons

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, AppState } from 'react-native';
+import { StyleSheet, Text, View, Button, AppState, SafeAreaView } from 'react-native';
 import { useEffect,useState,useRef } from 'react';
 import Slider from '@react-native-community/slider';
 import {bpmToMilliseconds} from "./utils/bpm.js"
 import BackgroundTimer from 'react-native-background-timer';
-
 import {setupAudio} from './utils/audio.js'
 import Metronome from './utils/metronome.js'
-
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function App() {
   // const [sound,setSound] = useState()
@@ -18,7 +17,6 @@ export default function App() {
   const intervalRef = useRef(0)
   const metronomeSpeed = bpmToMilliseconds(bpm,tempo)
   const appState = useRef(AppState.currentState)
-
 
   useEffect(()=> {
     setupAudio();
@@ -43,6 +41,7 @@ export default function App() {
     // clearInterval(intervalRef.current)
     // intervalRef.current = Metronome.play(volume,metronomeSpeed)
     BackgroundTimer.clearInterval(intervalRef.current)
+    // intervalRef.current = Metronome.playBackground(volume,metronomeSpeed)
     intervalRef.current = Metronome.playBackground(volume,metronomeSpeed)
   }
 
@@ -55,38 +54,94 @@ export default function App() {
     }
   }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title={isPlaying?"Stop":"Play"} onPress={metronomeOnPressHandler} ></Button>
+    <SafeAreaView style={styles.container} >
+      <View style={styles.box}>
+        <Text style={styles.heading}>Metronome App</Text>
+        <View style={styles.sliderBox}>
+        <Slider 
+          style={styles.slider} value={bpm}
+          minimumValue={120} maximumValue={240} step={1} onValueChange={(value)=>{setBpm(value)}}
+          minimumTrackTintColor='#000000' maximumTrackTintColor="#000000"
+        />
+        <Text style={styles.sliderText}>Bpm: {bpm}</Text>
+        <Slider 
+          style={styles.slider} step={0.1} value={volume}
+          minimumValue={0} maximumValue={1} onValueChange={(value)=>{setVolume(value)}}
+          minimumTrackTintColor='#000000' maximumTrackTintColor="#000000"
+        />
+        <Text style={styles.sliderText}>Volume: {volume.toFixed(1)}</Text>
+        <Slider 
+          style={styles.slider} step={4}
+          minimumValue={4} maximumValue={16} onValueChange={(value)=>{setTempo(value)}}
+          minimumTrackTintColor='#000000' maximumTrackTintColor="#000000"
+        />
+        <Text style={styles.sliderText}>Tempo: 1/{tempo}</Text>
+        <Icon size={50} style={styles.icon} name={isPlaying?'stop':'play-arrow'} onPress={metronomeOnPressHandler}></Icon>
+        </View>
+        
+      </View>
+      
       <StatusBar style="auto" />
-
-      <Slider 
-        style={{width:200,height:40}} value={bpm}
-        minimumValue={120} maximumValue={240} step={1} onValueChange={(value)=>{setBpm(value)}}
-        minimumTrackTintColor='#000000' maximumTrackTintColor="#000000"
-      />
-      <Text>Bpm: {bpm}</Text>
-      <Slider 
-        style={{width:200,height:40}} step={0.1} value={volume}
-        minimumValue={0} maximumValue={1} onValueChange={(value)=>{setVolume(value)}}
-        minimumTrackTintColor='#000000' maximumTrackTintColor="#000000"
-      />
-      <Text>Volume: {volume.toFixed(1)}</Text>
-      <Slider 
-        style={{width:200,height:40}} step={4}
-        minimumValue={4} maximumValue={16} onValueChange={(value)=>{setTempo(value)}}
-        minimumTrackTintColor='#000000' maximumTrackTintColor="#000000"
-      />
-      <Text>Tempo: 1/{tempo}</Text>
-    </View>
+      
+      
+      
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'coral',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  sliderBox: {
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  playButton: {
+    padding: 10
+  },
+  box: {
+    fontSize: 40,
+    fontWeight: "500",
+    fontFamily: 'notoserif',
+    backgroundColor:'silver',
+    width:'85%',
+    borderColor:'white',
+    borderWidth:10,
+    borderRadius:3,
+    // height:'60px',
+    // lineHeight:50,
+    flex:1,
+    // flexDirection:'column',
+    // flex: 1,
+    margin:50,
+    height:400,
+    
+    justifyContent:'center',
+    verticalAlign:'center',
+    textAlign:'center'
+  },
+  heading: {
+    fontSize: 40,
+    textAlign:'center',
+    paddingTop:20,
+  },
+  sliderText: {
+    fontSize:18,
+    textAlign:'center',
+    paddingBottom:50,
+  },
+  slider: {
+    width:200,
+    justifyContent:'center',
+    alignItems: 'center',
+    textAlign:'center',
+  },
+  icon: {
+    color:'green'
+  }
 });
